bac7a0f1247733755a1b411b7453294b8ee855bc
Saecki <tobiasschmitz2001@gmail.com>
2019-07-17 17:35:24 +0200

    Initial commit

---
+++ TS3Musicbot.py
@@ -0,0 +1,97 @@
+import pafy
+import vlc
+import os
+import asyncio
+import time
+
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+
+def getAllSubdirsOf(b='.'):
+	result = []
+	for d in os.listdir(b):
+		bd = os.path.join(b, d)
+		if os.path.isdir(bd): result.append(bd)
+	return result
+
+def getTS3ChannelChatFilePath():
+	p = os.getenv('APPDATA')
+	p += os.path.sep
+	p += "TS3Client\\chats"
+
+	all_subdirs = getAllSubdirsOf(p)
+	latest_subdir = max(all_subdirs, key=os.path.getmtime)
+
+	path = latest_subdir
+	path += os.path.sep
+	path += "channel.txt"
+
+	return path
+
+def getFileLineCount(path):
+    return sum(1 for line in open(path))
+
+def playAudioFromUrl(url):
+	
+	video = pafy.new(url)
+	best = video.getbest()
+	playurl = best.url
+	
+	Media = Instance.media_new(playurl)
+	Media.get_mrl()
+	player.set_media(Media)
+	player.play()
+
+def handleCommand(command):
+	if "!play" in command:
+		url = command.split("!play ", 1)[1]
+		url = url.replace("[URL]", "") 
+		url = url.replace("[/URL]", "")
+		playAudioFromUrl(url)
+		print("playing: " + url)
+	elif "!resume" in command:
+		player.play()
+		print("resumed")
+	elif "!pause" in command:
+		player.pause()
+		print("paused")
+	elif "!stop" in command:
+		player.stop()
+		print("stopped")
+	else:
+		print("The command: " + command + "has a wrong syntax")
+
+async def mainLoop():
+	global lastLine
+	while True:
+		currentLine = getFileLineCount(getTS3ChannelChatFilePath())
+		if  currentLine > lastLine:
+			lastLine = currentLine
+			with open(getTS3ChannelChatFilePath()) as f:
+				for line in f:
+					pass
+				command = line
+				handleCommand(command)
+
+		if player.get_state() == vlc.State.Ended:
+			player.stop()
+			print("song ended")
+
+		await asyncio.sleep(0.5)
+
+print("Waiting for a command")
+
+lastLine = getFileLineCount(getTS3ChannelChatFilePath())
+
+loop = asyncio.get_event_loop()
+loop.create_task(mainLoop())
+
+try:
+	loop.run_forever()
+except KeyboardInterrupt:
+	pass
+finally:
+	loop.run_until_complete(loop.shutdown_asyncgens())
+	loop.close()
+
+print("Exiting")
