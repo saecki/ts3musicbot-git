803c3a8ed11d25ba85ce3c90c2e520d421bf7a01
Saecki <tobiasschmitz2001@gmail.com>
2019-07-21 02:56:18 +0200

    added different play options and clear and delete functionality for playlists

--- TS3Musicbot.py
+++ TS3Musicbot.py
@@ -9,33 +9,33 @@ import random
 import urllib.request
 from bs4 import BeautifulSoup
 
-Instance = vlc.Instance()
-player = Instance.media_player_new()
-
 class Commands:
 	play = "!play"
+	playNext = "!playnext"
+	playNow = "!playnow"
+	playQueue = "!playqueue"
 	pause = "!pause"
 	next = "!next"
 	previous = "!prev"
 	stop = "!stop"
 	clear = "!clear"
 	shuffle = "!shuffle"
+	list = "!list"
 
 	repeat = "!repeat"
 	repeatAll = "all"
 	repeatStop = "stop"
-
-	queue = "!queue"
-	queuePlay = "play:"
 
 	playlist = "!playlist"
 	playlistCreate = "create:"
 	playlistFrom = "from:"
 	playlistAdd = "add:"
 	playlistTo = "to:"
+	playlistDelete = "delete:"
+	playlistPlay = "play:"
 	playlistQueue = "queue:"
-	playlistReplace = "replace"
 	playlistShuffle = "shuffle:"
+	playlistClear = "clear:"
 	playlistList = "list"
 
 class Command:
@@ -208,11 +208,19 @@ def getYoutubeURLFromString(string):
 #playback
 #
 
+def createVlcPlayer():
+	global Instance
+	global player
+
+	Instance = vlc.Instance()
+	player = Instance.media_player_new()
+
 def playSong():
 	if index < len(songQueue):
 		playAudioFromUrl(songQueue[index])
 
 def playAudioFromUrl(url):
+
 	try:
 		playurl = getBestYoutubeAudioURL(url)
 		
@@ -222,6 +230,7 @@ def playAudioFromUrl(url):
 		player.play()
 		print("playing " + url)
 	except:
+		createVlcPlayer()
 		print("couldn't play song with url " + url)
 
 #
@@ -229,36 +238,84 @@ def playAudioFromUrl(url):
 #
 
 def play(command):
-
 	if len(command.args) > 0:
-		if isURL(command.args[0].name):
-			url = getURL(command.args[0].name)
-			if isYoutubeURL(url):
-				songQueue.append(url)
-				print("added " + url + " to the queue")
-				if not player.get_state() == vlc.State.Playing or player.get_state() == vlc.State.Paused:
-					playSong()
-			else:
-				print("specified value is no youtube url")
-		else:
-			string = ""
-			for arg in command.args:
-				string += arg.name + " "
-
-			url = getYoutubeURLFromString(string)
+		url = getYoutubeURLFromCommand(command)
+		
+		if not url == None:
+			songQueue.append(url)
+			print("added " + url + " to the queue")
+			if not (player.get_state() == vlc.State.Playing or player.get_state() == vlc.State.Paused or len(songQueue) == 1):
+				next()
 			
-			if not url == None:
-				songQueue.append(url)
-				print("added " + url + " to the queue")
-				if not player.get_state() == vlc.State.Playing or player.get_state() == vlc.State.Paused:
-					playSong()
-			else:
-				print("couldn't find any video")
+			if len(songQueue) == 1:
+				playSong()
+
 	elif player.get_state() == vlc.State.Paused:
 		player.play()
 		print("resumed")
 	else:
 		playSong()
+
+def playNext(command):
+	if len(command.args) > 0:
+		url = getYoutubeURLFromCommand(command)
+		
+		if not url == None:
+			songQueue.insert(index + 1, url)
+			print("added " + url + "as next song to the queue")
+	else:
+		print("not enough arguments")
+
+def playNow(command):
+	if len(command.args) > 0:
+		url = getYoutubeURLFromCommand(command)
+		
+		if not url == None:
+			songQueue.insert(index + 1, url)
+			if len(songQueue) == 1:
+				playSong()
+			else:
+				next()
+	else:
+		print("not enough arguments")
+
+def playQueue(command):
+	global index
+
+	if len(command.args) > 0:
+		integer = True
+
+		try:
+			tempIndex = int(command.args[0].name)
+		except:
+			integer = False
+			print("enter a valid number as argument")
+
+		if integer:
+			index = getNumberBetween(tempIndex, 0, len(songQueue) - 1)
+			playSong()
+			print("playing queue at index " + str(index))
+
+def getYoutubeURLFromCommand(command):
+	if isURL(command.args[0].name):
+		url = getURL(command.args[0].name)
+		if isYoutubeURL(url):
+			return url
+		else:
+			print("specified value is no youtube url")
+	else:
+		string = ""
+		for arg in command.args:
+			string += arg.name + " "
+
+		url = getYoutubeURLFromString(string)
+		
+		if not url == None:
+			return url
+		else:
+			print("couldn't find any video")
+	
+	return None
 
 def pause():
 	player.pause()
@@ -311,6 +368,11 @@ def shuffle():
 	random.shuffle(songQueue)
 	print("shuffled queue")
 
+def list():
+	print("queue:")
+	for s in songQueue:
+		print("   " + s)
+
 def repeat(command):
 	global repeatSong
 
@@ -327,17 +389,6 @@ def repeat(command):
 		repeatSong = 1
 		print("repeating one song")
 
-def queue(command):
-	global index
-
-	if len(command.args) > 0:
-		if command.args[0].name == Commands.queuePlay:
-			tempIndex = int(command.args[0].value)
-			index = getNumberBetween(tempIndex, 0, len(songQueue) - 1)
-			playSong()
-			print("playing queue at index " + str(index))
-		else:
-			print("argument " + command.args[0].name + " not found")
 #
 #playlist
 #
@@ -346,12 +397,18 @@ def playlist(command):
 	if len(command.args) > 0:
 		if command.args[0].name == Commands.playlistCreate:
 			playlistCreate(command.args)
+		elif command.args[0].name == Commands.playlistDelete:
+			playlistDelete(command.args)
 		elif command.args[0].name == Commands.playlistAdd:
 			playlistAdd(command.args)
+		elif command.args[0].name == Commands.playlistPlay:
+			playlistPlay(command.args)
 		elif command.args[0].name == Commands.playlistQueue:
 			playlistQueue(command.args)
 		elif command.args[0].name == Commands.playlistShuffle:
 			playlistShuffle(command.args)
+		elif command.args[0].name == Commands.playlistClear:
+			playlistClear(command.args)
 		elif command.args[0].name == Commands.playlistList:
 			playlistList()
 		else:
@@ -383,6 +440,15 @@ def playlistCreate(args):
 		playlists.append(Playlist(args[0].value))
 		print("created playlist " + args[0].value)
 
+def playlistDelete(args):
+	for p in playlists:
+		if p.name == args[0].value:
+			playlists.remove(p)
+			print("deleted " + args[0].value)
+
+			return
+	print("playlist not found")
+
 def playlistAdd(args):
 	if len(args) > 1:
 		if args[1].name == Commands.playlistTo:			
@@ -404,24 +470,28 @@ def playlistAdd(args):
 	else:
 		print("not enough arguments")
 
+def playlistPlay(args):
+	global songQueue
+
+	for p in playlists:
+		if p.name == args[0].value:
+			clear()
+			songQueue = p.songURLs.copy()
+			print("replaced the queue with " + p.name)
+			playSong()
+
+			return
+	print("playlist not found")
+
 def playlistQueue(args):
 	global songQueue
 
 	for p in playlists:
 		if p.name == args[0].value:
-			if len(args) > 1:
-				if args[1].name == Commands.playlistReplace:
-					clear()
-					songQueue = p.songURLs.copy()
-					print("replaced the queue with " + p.name)
-					playSong()
-				else:
-					print("specified argument not correct")
-			else:
-				songQueue = songQueue + p.songURLs
-				print("added songs from " + p.name + " to the queue")
-				if not player.get_state() == vlc.State.Playing or player.get_state() == vlc.State.Paused:
-					playSong()
+			songQueue = songQueue + p.songURLs
+			print("added songs from " + p.name + " to the queue")
+			if not (player.get_state() == vlc.State.Playing or player.get_state() == vlc.State.Paused):
+				playSong()
 
 			return
 	print("playlist not found")
@@ -434,13 +504,24 @@ def playlistShuffle(args):
 			return
 	print("playlist not found")
 
+def playlistClear(args):
+	for p in playlists:
+		if p.name == args[0].value:
+			p.songURLs.clear()
+			print("cleared playlist")
+			return
+	print("playlist not found")
+
 def playlistList():
 	print("playlists:")
 	for p in playlists:
+		index = 0
+
 		print()
 		print(p.name)
 		for s in p.songURLs:
-			print("   " + s)
+			print("(" + str(index) + ")   " + s)
+			index += 1
 
 #
 #commands
@@ -488,6 +569,12 @@ def handleCommand(string):
 	if not command == None:
 		if command.name == Commands.play:
 			play(command)
+		elif command.name == Commands.playNext:
+			playNext(command)
+		elif command.name == Commands.playNow:
+			playNow(command)
+		elif command.name == Commands.playQueue:
+			playQueue(command)
 		elif command.name == Commands.pause:
 			pause()
 		elif command.name == Commands.previous:
@@ -500,10 +587,10 @@ def handleCommand(string):
 			clear()
 		elif command.name == Commands.shuffle:
 			shuffle()
+		elif command.name == Commands.list:
+			list()
 		elif command.name == Commands.repeat:
 			repeat(command)
-		elif command.name == Commands.queue:
-			queue(command)
 		elif command.name == Commands.playlist:
 			playlist(command)
 		else:
@@ -517,35 +604,42 @@ async def mainLoop():
 	global index
 
 	while True:
-		currentLine = getFileLineCount(getTS3ChannelChatFilePath())
-		if  currentLine > lastLine:
-			with open(getTS3ChannelChatFilePath()) as f:
-				for line in f:
-					pass
-				command = line
-				handleCommand(command)
-				lastLine = currentLine
-		elif player.get_state() == vlc.State.Ended:
-			if repeatSong == 0:
-				next()
-			if repeatSong == 1:
-				playSong()
-			elif repeatSong == 2:
-				if index >= len(songQueue) - 1:
-					index = 0
-					playSong()
-				else:
+		try:
+			currentLine = getFileLineCount(getTS3ChannelChatFilePath())
+			if  currentLine > lastLine:
+				with open(getTS3ChannelChatFilePath()) as f:
+					for line in f:
+						pass
+					command = line
+					handleCommand(command)
+					lastLine = currentLine
+			elif player.get_state() == vlc.State.Ended:
+				if repeatSong == 0:
 					next()
+				if repeatSong == 1:
+					playSong()
+				elif repeatSong == 2:
+					if index >= len(songQueue) - 1:
+						index = 0
+						playSong()
+					else:
+						next()
+		except:
+			print("error in main loop")
 
 		await asyncio.sleep(0.5)
 
-playlists = []
+Instance = None
+player = None
+createVlcPlayer()
 
-readData()
+playlists = []
 
 songQueue = []
 index = 0
 repeatSong = 0
+
+readData()
 
 lastLine = getFileLineCount(getTS3ChannelChatFilePath())
 
